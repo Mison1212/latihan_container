2f8f48f40d2241b9ed5a9e8f61a2ec7fcc961e67
Mison1212 <misonwenda20@gmail.com>
2025-10-24 10:36:07 +0700

    latihan container

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Judul',
       home: MyHome(),
-        
-      );
+    );
   }
 }
 
@@ -30,15 +29,94 @@ class MyHome extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
         actions: [
-          IconButton(onPressed: (){
-            print('menu button pressed');
-          }, 
-          icon: const Icon(Icons.menu),
+          IconButton(
+            onPressed: () {
+              print('menu button pressed');
+            },
+            icon: const Icon(Icons.menu),
           ),
-          ],
+        ],
       ),
-      body: Container(
-        color: Colors.cyan,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              // color: Colors.cyan,
+              alignment: Alignment.topCenter,
+              // child:Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     Icon(Icons.person),
+              //     SizedBox(width: 10),
+              //     Icon(Icons.account_balance),
+              //     Text("ini adalah data"),
+              //   ],
+              // ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.blue, Colors.red],
+                ),
+              ),
+            ),
+            SizedBox(width: 50), // jarak atau spasi box
+            Container(
+              width: 100,
+              height: 100,
+              // color: Colors.cyan,
+              alignment: Alignment.topCenter,
+              // child:Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     Icon(Icons.person),
+              //     SizedBox(width: 10),
+              //     Icon(Icons.account_balance),
+              //     Text("ini adalah data"),
+              //   ],
+              // ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.blue, Colors.yellow],
+                ),
+              ),
+            ),
+            SizedBox(width: 50),// jarak atau spasi box
+            Container(
+              width: 100,
+              height: 100,
+              // color: Colors.cyan,
+              alignment: Alignment.topCenter,
+              // child:Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     Icon(Icons.person),
+              //     SizedBox(width: 10),
+              //     Icon(Icons.account_balance),
+              //     Text("ini adalah data"),
+              //   ],
+              // ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.blue, Colors.greenAccent],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
